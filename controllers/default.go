81ae1e255d69106453b00682fc4e3efeac8fb44a
yanglf2 <yanglf2@lenovo.com>
2014-04-16 15:52:23 +0800

    add models and controllers

--- controllers/default.go
+++ controllers/default.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"fmt"
+	"upyun"
+
 	"github.com/astaxie/beego"
 )
 
@@ -9,7 +12,23 @@ type MainController struct {
 }
 
 func (this *MainController) Get() {
-	this.Data["Website"] = "beego.me"
-	this.Data["Email"] = "astaxie@gmail.com"
+
+	u := upyun.NewUpYun("backup", "zerob13", "www.zerob13.in")
+
+	u.Debug = true
+	u.SetApiDomain(upyun.EdAuto)
+	v, err := u.GetBucketUsage()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	dirs, err := u.ReadDir("/")
+	fmt.Printf("ReadDir: %v\n", err)
+	for i, d := range dirs {
+		fmt.Printf("\t%d: %v\n", i, d)
+	}
+
+	this.Data["Website"] = v / 1024 / 1024
+	this.Data["Email"] = dirs
 	this.TplNames = "index.tpl"
 }
--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	_ "upyun_sync/routers"
+
 	"github.com/astaxie/beego"
 )
 
 func main() {
 	beego.Run()
 }
-
